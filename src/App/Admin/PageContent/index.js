875e66baa5049b05cc4f4dff7b6a528cdc65c50e
Geoff Lawson <geoff.lawson@build.com>
2018-02-21 22:46:18 -0800

    123: adds admin accounts

---
+++ src/App/Admin/PageContent/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PageContent = () => {
+
+    return (
+        <div className="section">
+            <h2>Page Content</h2>
+        </div>
+    );
+};
+
+export default PageContent;
