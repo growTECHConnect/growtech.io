3cc54802ae084756f2eafa638f695fd40c1a0e8b
Spartan005 <jcoppy005@gmail.com>
2019-08-13 13:54:05 -0700

    made NewsContent a class

--- src/App/Admin/NewsContent/index.tsx
+++ src/App/Admin/NewsContent/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import '../styles.css';
 
-const NewsContent = () => {
-    return (
-        <div className="section">
-            <h2>News Content</h2>
-        </div>
-    );
-};
+class NewsContent extends React.Component {
+    render() {
+        return (
+            <div className="section">
+                <h2>News Content</h2>
+            </div>
+        );
+    }
+}
 
 export default NewsContent;
